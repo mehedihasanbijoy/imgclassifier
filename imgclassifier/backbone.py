64f74769328c1f1a8793467921e11c60b613cfc0
Mehedi Hasan Bijoy <bijoyx3@gmail.com>
2021-11-20 00:44:04 +0600

    backbone

--- imgclassifier/backbone.py
+++ imgclassifier/backbone.py
@@ -1,2 +1,15 @@
-def backbone():
-    print('Inside backbone file...')
+import torch, torchvision
+
+
+def resnet18(num_classes):
+    '''
+        out_embd_size: size of the feature vector. i.e. embedding.shape[0] = 400
+    '''
+    model = torchvision.models.resnet18(pretrained=True)
+
+    model.fc = torch.nn.Linear(
+        in_features=model.fc.in_features,
+        out_features=num_classes
+    )
+
+    return model
--- imgclassifier/empty.py
+++
@@ -1,2 +0,0 @@
-def empty():
-    print('Inside empty file...')
