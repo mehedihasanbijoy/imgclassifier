37cdc72c3249a6c32997ce74f73739eb0b613a14
Mehedi Hasan Bijoy <bijoyx3@gmail.com>
2021-11-29 00:10:02 +0600

    Updated

--- imgclassifier/__init__.py
+++ imgclassifier/__init__.py
@@ -1,9 +1,3 @@
-# import torch
-# import torchvision
-
-# from torch.utils.data import DataLoader
-# from torchvision.datasets import ImageFolder
-# import os
 from imgclassifier.backbone import resnet18
 from imgclassifier.train import train
 from imgclassifier.utils import evaluation_report
--- imgclassifier/backbone.py
+++ imgclassifier/backbone.py
@@ -1,15 +1,98 @@
 import torch, torchvision
 
 
-def resnet18(num_classes):
-    '''
-        out_embd_size: size of the feature vector. i.e. embedding.shape[0] = 400
-    '''
-    model = torchvision.models.resnet18(pretrained=True)
+def alexnet(num_classes):
+    pass
 
+
+def vgg11(num_classes):
+    pass
+
+
+def vgg13(num_classes):
+    pass
+
+
+def vgg16(num_classes):
+    pass
+
+
+def vgg19(num_classes):
+    pass
+
+
+def resnet18(num_classes):
+    model = torchvision.models.resnet18(pretrained=True)
     model.fc = torch.nn.Linear(
         in_features=model.fc.in_features,
         out_features=num_classes
     )
-
     return model
+
+
+def resnet34(num_classes):
+    pass
+
+
+def resnet50(num_classes):
+    pass
+
+
+def resnet101(num_classes):
+    pass
+
+
+def resnet152(num_classes):
+    pass
+
+
+def densenet121(num_classes):
+    pass
+
+
+def densenet161(num_classes):
+    pass
+
+
+def densenet169(num_classes):
+    pass
+
+
+def densenet201(num_classes):
+    pass
+
+
+def mobilenet_v2(num_classes):
+    pass
+
+
+def mobilenet_v3_large(num_classes):
+    pass
+
+
+def mobilenet_v3_small(num_classes):
+    pass
+
+
+def mnasnet_1(num_classes):
+    pass
+
+
+def mnasnet_p5(num_classes):
+    pass
+
+
+def shufflenet_v2_1(num_classes):
+    pass
+
+
+def shufflenet_v2_p5(num_classes):
+    pass
+
+
+def resnext50_4d(num_classes):
+    pass
+
+
+def resnext101_8d(num_classes):
+    pass
